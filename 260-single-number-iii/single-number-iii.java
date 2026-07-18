3c7ea6ea93a886b4b70323b01c3127466d4d5014
Progamer8077 <131904273+Progamer8077@users.noreply.github.com>
2026-07-18 15:01:06 +0530

    Time: 8 ms (15.56%) | Memory: 48.2 MB (8.98%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int[] result = new int[2];
+        int index = 0;
+        
+        int i = 0;
+        while (i < nums.length) {
+            if (i == nums.length - 1 || nums[i] != nums[i + 1]) {
+                result[index++] = nums[i];
+                i++;
+            } else {
+                i += 2;
+            }
+        }
+        
+        return result;
+    }
+}
